03b6e6eb464cad97e475dddae6c85307706dc7ec
mickeysrishti <Mukeshpathak345@gmail.com>
2024-08-13 00:10:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HelloMsg from './components/HelloMsg'
+
+function App() {
+
+  return (
+    <>
+      <HelloMsg userName="Mukesh" textColor="red"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HelloMsg.jsx
@@ -0,0 +1,15 @@
+function HelloMsg({userName, textColor}) {
+
+    let styles = {
+        color: textColor,
+    }
+    return (
+    <>
+    <h2 style={styles}>Hello {userName}</h2>
+
+    </>
+    )
+}
+
+
+export default HelloMsg;
